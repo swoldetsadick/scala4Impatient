a32372d2694c202439c30ee49015604ccf9c3929
swoldetsadick <kebrab67@gmail.com>
2018-12-26 03:17:12 +0100

    finished chapter 2

--- chapter2/src/main/scala/Chapter02.scala
+++ chapter2/src/main/scala/Chapter02.scala
@@ -77,4 +77,70 @@ class Chapter02 {
     *  0
     */
 
+  /**
+    *  Exercice 6
+    *
+    *  scala> var prod1: Int = 1
+    *  prod1: Int = 1
+    *  scala> var prod2: BigInt = 1
+    *  prod2: scala.math.BigInt = 1
+    *  scala> for( c <- "Hello"; l = c.toLong) prod1 *= l.toInt;
+    *  scala> for( c <- "Hello"; l = c.toLong) prod2 *= l;
+    *  scala> print(prod1)
+    *  825152896
+    *  scala> print(prod2)
+    *  9415087488
+    *
+    *  Int is not well suited here. A better suited type would be BigInt or Long. Integer overflow.
+    */
+
+  /**
+    * Exercice 7
+    *
+    * scala> "Hello".map(_.toLong).foldLeft(1: BigInt)(_*_)
+    * res0: scala.math.BigInt = 9415087488
+    * scala> "Hello".map(_.toLong).foldLeft(1: Long)(_*_)
+    * res1: Long = 9415087488
+    * scala> "Hello".map(_.toLong.toInt).foldLeft(1: Int)(_*_)
+    * res2: Int = 825152896
+    */
+
+  /**
+    *  Exercice 8
+    *  scala> def product(arg: String): Long = {
+    *       | var result: Long = 1
+    *       | for( c <- arg; l = c.toLong) result *= l
+    *       | result
+    *       | }
+    *  product: (arg: String)Long
+    *  scala> product("Hello")
+    *  res0: Long = 9415087488
+    */
+
+  /**
+    *  Exercice 9
+    *  scala> def product(arg: String): Long = {
+    *       | if (arg.length == 0) 0
+    *       | else if (arg.length == 1) arg(0).toLong
+    *       | else arg(0).toLong * prod(arg.drop(1))
+    *       | }
+    *  product: (arg: String)Long
+    *  scala> product("Hello")
+    *  res0: Long = 9415087488
+    */
+
+  /**
+    *  Exercice 10
+    *
+    *  scala> def powered(x: Double, n: Int): Double = {
+    *       | if (n == 0) 1
+    *       | else if (n < 0) (1.0 / powered(x, n * -1))
+    *       | else if (n % 2 == 1) (x * powered(x, n - 1))
+    *       | else (powered(x, n / 2) * powered(x, n / 2))
+    *       | }
+    *  powered: (x: Double, n: Int)Double
+    *  scala> powered(2, 3)
+    *  res0: Double = 8
+    */
+
 }
