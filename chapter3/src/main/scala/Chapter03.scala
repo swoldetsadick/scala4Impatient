1c9050c4c0a853b45d5ac1c8f397e381abb7b31b
swoldetsadick <kebrab67@gmail.com>
2018-12-26 15:04:33 +0100

    chapter 3 exercices

--- chapter3/src/main/scala/Chapter03.scala
+++ chapter3/src/main/scala/Chapter03.scala
@@ -3,4 +3,75 @@
   */
 object Chapter03 {
 
+  /**
+    *  Exercice 1
+    *
+    *  scala> n: Int = 10
+    *  n: Int = 10
+    *  scala> val rang = 0 until n
+    *  rang: scala.collection.immutable.Range = Range(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    *  scala> val a = rang.toArray
+    *  a Array[Int] = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    */
+
+  /**
+    *  Exercice 2
+    *
+    *  scala> val rang = 0 until 10
+    *  rang: scala.collection.immutable.Range = Range(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    *  scala> val a = rang.toArray
+    *  a Array[Int] = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    *  scala> for(i <- 0 until a.length) if (i % 2 == 0) {var b = a(i); a(i) = a(i+1); a(i + 1) = b}
+    *  scala> a
+    *  res0: Array[Int] = Array(1, 0, 3, 2, 5, 4, 7, 6, 9, 8)
+    */
+
+  /**
+    *  Exercice 3
+    *
+    *  scala>
+    */
+
+  /**
+    *  Exercice 4
+    *
+    *
+    */
+
+  /**
+    *  Exercice 5
+    *
+    *
+    */
+
+  /**
+    *  Exercice 6
+    *
+    *
+    */
+
+  /**
+    *  Exercice 7
+    *
+    *
+    */
+
+  /**
+    *  Exercice 8
+    *
+    *
+    */
+
+  /**
+    *  Exercice 9
+    *
+    *
+    */
+
+  /**
+    *  Exercice 10
+    *
+    *
+    */
+
 }
