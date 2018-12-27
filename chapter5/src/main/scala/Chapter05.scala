21c5bc245c4b34eb7fb4a0bc40eff725a5228396
swoldetsadick <kebrab67@gmail.com>
2018-12-27 22:03:45 +0100

    starting chapter 5

--- chapter5/src/main/scala/Chapter05.scala
+++ chapter5/src/main/scala/Chapter05.scala
@@ -3,4 +3,64 @@
   */
 object Chapter05 {
 
+  /**
+    * Exercice 1
+    *
+    *
+    */
+
+  /**
+    * Exercice 2
+    *
+    *
+    */
+
+  /**
+    * Exercice 3
+    *
+    *
+    */
+
+  /**
+    * Exercice 4
+    *
+    *
+    */
+
+  /**
+    * Exercice 5
+    *
+    *
+    */
+
+  /**
+    * Exercice 6
+    *
+    *
+    */
+
+  /**
+    * Exercice 7
+    *
+    *
+    */
+
+  /**
+    * Exercice 8
+    *
+    *
+    */
+
+  /**
+    * Exercice 9
+    *
+    *
+    */
+
+  /**
+    * Exercice 10
+    *
+    *
+    */
+
 }
